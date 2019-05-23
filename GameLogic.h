69ee69ad4b247bf2973d42e4b14e49a4056f35b7
Lee Si Hyun <lsh9034@kookmin.ac.kr>
2019-05-23 12:31:15 +0900

    GameLogic

---
+++ GameLogic.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<cstdlib>
+using namespace std;
+class GameLogic
+{
+	int step;
+	int push;
+	int **map;
+	vector< pair<int,int> > destination;
+	int R;
+	int C;
+	int numDestination;
+public:
+	GameLogic(int **map, int R, int C);
+	void Move(int direction);
+	bool isGameclear();
+	int getStep();
+	int getPush();
+	bool isMap(int i, int j);
+};
