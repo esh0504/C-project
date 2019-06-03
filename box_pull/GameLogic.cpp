a3a3477c2d3e06de169abd2d9e3ee6c690eb14e1
음승호(seung ho - eum) <39682743+esh0504@users.noreply.github.com>
2019-06-03 13:56:07 +0900

    all clear

---
+++ box_pull/GameLogic.cpp
@@ -0,0 +1,173 @@
+#include "GameLogic.h"
+using namespace std;
+#define EMPTY 0
+#define WALL 1
+#define BOX 2
+#define DESTINATION 3
+#define OUTSIDE 4
+#define DESBOX 5
+#define CHARACTER 6
+#define DESCHARACTER 7
+#define LEFT 4
+#define RIGHT 6
+#define UP 8
+#define DOWN 2
+typedef pair<int, int> pii;
+pair<int, int> operator +(pair<int, int> &i, pair<int, int> &j)
+{
+	pair<int,int> r = pair<int,int>(i.first + j.first,i.second + j.second);
+	return r;
+}
+
+int Convert_dir(int direction)
+{
+	if (direction == LEFT)return 0;
+	if (direction == UP)return 1;
+	if (direction == RIGHT)return 2;
+	if (direction == DOWN)return 3;
+	return -1;
+}
+
+GameLogic::GameLogic(int **map, int R, int C) :step(0), push(0)
+{
+	this->map = map;
+	this->R = R;
+	this->C = C;
+	for (int i = 0; i < R; i++)
+		for (int j = 0; j < C; j++)
+			if (map[i][j] == DESTINATION)
+				destination.push_back(pii(i,j));
+	this->numDestination = destination.size();
+}
+
+bool GameLogic::isMap(int i, int j)
+{
+	if (i < 0 || i >= R || j < 0 || j >= C)return false;
+	return true;
+}
+void GameLogic::Move(int direction)
+{
+	pii dir[4];
+	dir[Convert_dir(LEFT)] = { 0,-1 };
+	dir[Convert_dir(UP)] = { -1,0 };
+	dir[Convert_dir(RIGHT)] = { 0,1 };
+	dir[Convert_dir(DOWN)] = { 1,0 };
+	
+	pii *myPos=NULL;
+	for (int i = 0; i < R; i++)
+		for (int j = 0; j < C; j++)
+			if (map[i][j] == CHARACTER || map[i][j]==DESCHARACTER)
+			{
+				myPos = new pii(i,j);
+				break;
+			}
+
+	if (!myPos)
+	{
+		//cout << "캐릭터가 맵 안에 없습니다.\n";
+		return;
+	}
+
+	direction = Convert_dir(direction);
+	if (direction == -1)
+	{
+		//cout << "올바른 방향을 입력하지 않았습니다\n";
+		return;
+	}
+	pii movePos = *myPos + dir[direction];
+	if (!isMap(myPos->first, myPos->second))
+		return;
+	else if (map[movePos.first][movePos.second] == EMPTY || map[movePos.first][movePos.second]==DESTINATION)
+	{
+		map[myPos->first][myPos->second] = EMPTY;
+		map[movePos.first][movePos.second] = CHARACTER;
+		++step;
+	}
+	else if (map[movePos.first][movePos.second] == BOX || map[movePos.first][movePos.second] == DESBOX)
+	{
+		pii movePos2 = movePos + dir[direction];
+		if (!isMap(movePos2.first, movePos2.second))return;
+		else if (map[movePos2.first][movePos2.second] == EMPTY || map[movePos2.first][movePos2.second]==DESTINATION)
+		{
+			map[movePos2.first][movePos2.second] = BOX;
+			map[movePos.first][movePos.second] = CHARACTER;
+			map[myPos->first][myPos->second] = EMPTY;
+			++step;
+			++push;
+		}
+	}
+	for (int i = 0; i < numDestination; i++)
+	{
+		pii pos = destination[i];
+		if (map[pos.first][pos.second] == EMPTY)
+			map[pos.first][pos.second] = DESTINATION;
+		else if (map[pos.first][pos.second] == CHARACTER)
+			map[pos.first][pos.second] = DESCHARACTER;
+		else if (map[pos.first][pos.second] == BOX)
+			map[pos.first][pos.second] = DESBOX;
+	}
+	return;
+}
+bool GameLogic::isGameclear()
+{
+	for (int i = 0; i < destination.size(); i++)
+	{
+		pii pos = destination[i];
+		if (map[pos.first][pos.second] != DESBOX)return false;
+	}
+	return true;
+}
+int GameLogic::getStep()
+{
+	return step;
+}
+int GameLogic::getPush()
+{
+	return push;
+}
+void GameLogic::printMap()
+{
+	for (int i = 0; i < R; i++)
+	{
+		for (int j = 0; j < C; j++)
+			cout << map[i][j];
+		cout << "\n";
+	}
+}
+/*
+int main(void)
+{
+	ifstream input("input.txt");
+	int r, c;
+	pii pos;
+	input >> r >> c;
+	input >> pos.first >> pos.second;
+	if (input.fail())return 0;
+	int **map = new int*[r];
+	for (int i = 0; i < r; i++)
+	{
+		map[i] = new int[c];
+		for (int j = 0; j < c; j++)
+		{
+			input >> map[i][j];
+		}
+	}
+	map[pos.first][pos.second] = CHARACTER;
+	GameLogic gl(map,r,c);
+	while (1)
+	{
+		for (int i = 0; i < r; i++)
+		{
+			for (int j = 0; j < c; j++)
+				cout << map[i][j];
+			cout << "\n";
+		}
+		int dir;
+		cin >> dir;
+		gl.Move(dir);
+		system("cls");
+		if (gl.isGameclear())break;
+	}
+	return 0;
+}
+*/
---
+++ box_pull/UI.cpp
@@ -0,0 +1,68 @@
+#include <ncurses.h>
+#include <stdlib.h>
+#include <string>
+
+//대상별 색상인덱스 설정
+#define EMPTY 0
+#define WALL 1
+#define BOX 2
+#define DESTINATION 3
+#define OUTSIDE 4
+#define DESBOX 5
+#define CHARACTER 6
+#define DESCHARACTER 7
+
+class UI
+{
+	std::string char_list = ".^#x~+o*";
+public:
+	UI()
+	{
+		initscr();
+		curs_set(0);		   //실행 시 커서 비활성화
+		if (has_colors() == FALSE) //사용자의 터미널(커맨드)에서 색상을 지원하는지 여부확인
+		{
+			endwin();
+			printf("Your terminal does not support color\n");
+			exit(1);
+		}
+		start_color();				      // Color 사용 선언
+		if (init_color(COLOR_BLUE, 0, 0, 300) == ERR) //터미널에서 색상이 정상적으로 바뀌는지 여부확인
+		{
+			printw("Your terminal cannot change the color definitions\n");
+			printw("press any key to continue...\n");
+			getch();
+			move(0, 0);
+		}
+		init_pair(EMPTY, COLOR_WHITE, COLOR_BLACK);	//0(빈칸):  하양, 검정
+		init_pair(WALL, COLOR_BLACK, COLOR_WHITE);	 //1(벽): 검정, 노랑
+		init_pair(BOX, COLOR_MAGENTA, COLOR_YELLOW);       //2(상자): 분홍, 노랑
+		init_pair(DESTINATION, COLOR_YELLOW, COLOR_BLACK); //3(목표): 노랑, 검정
+		init_pair(OUTSIDE, COLOR_CYAN, COLOR_BLUE);	//4(바깥): 형광, 파랑
+		init_pair(DESBOX, COLOR_YELLOW, COLOR_RED);	//5(목표+상자): 노랑, 빨강
+		init_pair(CHARACTER, COLOR_RED, COLOR_MAGENTA);    //6(캐릭터) 빨강, 분홍
+		init_pair(DESCHARACTER, COLOR_CYAN, COLOR_CYAN);   //7(목표+캐릭터):  형광, 형광
+	}
+	void menu(int s, int p)
+	{
+		printw("character-step count: %d, box-push count: %d", s, p);
+		printw("\n\n");
+	}
+	void draw(int **map, int step, int push)
+	{
+		clear();
+		menu(step, push);
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				int index = map[i][j];
+				attron(COLOR_PAIR(index));
+				printw(" %c ", char_list[index]);
+				attroff(COLOR_PAIR(index));
+			}
+			printw("\n");
+		}
+		refresh();
+	}
+};
